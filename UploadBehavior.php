668041553c564c98e5b8466d87015ad22e3d13fd
colee <5969226@qq.com>
2016-04-07 15:11:19 +0800

    init

---
+++ UploadBehavior.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * ==============================================
+ * Copy right 2015-2016
+ * ----------------------------------------------
+ * This is not a free software, without any authorization is not allowed to use and spread.
+ * ==============================================
+ * @param unknowtype
+ * @return return_type
+ * @author: CoLee
+ */
+namespace colee\behavior;
+
+use yii\base\Behavior;
+class UploadBehavior extends Behavior
+{
+    
+}
